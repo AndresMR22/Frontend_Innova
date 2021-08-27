7bc220812c1218653554669eab8e23c302947756
Andres <gamr98@outlook.es>
2021-08-27 15:09:50 -0500

    comentario

--- AndresMR2018-WebSite-Angular-Graphics_PowerBI-master/src/server.js
+++ AndresMR2018-WebSite-Angular-Graphics_PowerBI-master/src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/AndresMR2018-WebSite-Angular-Graphics_PowerBI-master'));
-app.get('/',function(req,res){
-res.sendFile(path.join(__dirname+'/dist/AndresMR2018-WebSite-Angular-Graphics_PowerBI-master/index.html'));
+app.use(express.static(__dirname+'/dist'));
+app.all('*',function(req,res){
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 app.listen(process.env.PORT || 5000);
