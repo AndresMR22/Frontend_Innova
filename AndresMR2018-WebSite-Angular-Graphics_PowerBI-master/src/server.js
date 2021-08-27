a80ffaa7b693a5cf2476d172b2cfee3e534c2d6e
Andres <gamr98@outlook.es>
2021-08-27 12:17:27 -0500

    configuracion server

--- AndresMR2018-WebSite-Angular-Graphics_PowerBI-master/src/server.js
+++ AndresMR2018-WebSite-Angular-Graphics_PowerBI-master/src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/my-dream-app'));
+app.use(express.static(__dirname+'/dist/AndresMR2018-WebSite-Angular-Graphics_PowerBI-master'));
 app.get('/',function(req,res){
-res.sendFile(path.join(__dirname+'/dist/my-dream-app/index.html'));
+res.sendFile(path.join(__dirname+'/dist/AndresMR2018-WebSite-Angular-Graphics_PowerBI-master/index.html'));
 });
 
 app.listen(process.env.PORT || 5000);
